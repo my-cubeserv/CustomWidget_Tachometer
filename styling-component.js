b31b914ac0c7d071abef8b0c4dc698a087c620e6
my-cubeserv <113102922+my-cubeserv@users.noreply.github.com>
2023-11-23 08:53:55 +0100

    Update styling-component.js

--- styling-component.js
+++ styling-component.js
@@ -11,39 +11,11 @@
                 <td>Font size</td>\
                 <td><input id="ap_fontsize" type="number" name="fontSize" max="30" min="6"></td>\
             </tr>\
-			<tr>\
-                <td>select Top</td>\
-                <td><input id="ap_selectedTop" type="number" name="selectedTop"  max="6" min="1"></td>\
-            </tr>\
-			 <tr>\
-                <td>Background color top 1</td>\
-                <td><input id="ap_bgcolor1" type="color" name="bgColor1"></td>\
-            </tr>\
-			<tr>\
-                <td>Background color top 2</td>\
-                <td><input id="ap_bgcolor2" type="color" name="bgColor2"></td>\
-            </tr>\
-			<tr>\
-                <td>Background color top 3</td>\
-                <td><input id="ap_bgcolor3" type="color" name="bgColor3"></td>\
-            </tr>\
-			<tr>\
-                <td>Background color top 4</td>\
-                <td><input id="ap_bgcolor4" type="color" name="bgColor4"></td>\
-            </tr>\
-			<tr>\
-                <td>Background color top 5</td>\
-                <td><input id="ap_bgcolor5" type="color" name="bgColor5"></td>\
-            </tr>\
-			<tr>\
-                <td>Background color top 6</td>\
-                <td><input id="ap_bgcolor6" type="color" name="bgColor6"></td>\
-            </tr>\
             </table>\
         </form>\
     ';
 
-    class PyramidChartProperties extends HTMLElement {
+    class TachometerProperties extends HTMLElement {
         constructor() {
             super();
             this._shadowRoot = this.attachShadow({ mode: 'open' });
@@ -61,12 +33,6 @@
 								value: this.value,
                                 color: this.color,
 								fontsize: this.fontsize,
-								bgColor1: this.bgColor1,
-								bgColor2: this.bgColor2,
-								bgColor3: this.bgColor3,
-								bgColor4: this.bgColor4,
-								bgColor5: this.bgColor5,
-								bgColor6: this.bgColor6
                             }
                         }
                     }));
@@ -83,12 +49,6 @@
                         value: this.value,
                         color: this.color,
 						fontsize: this.fontsize,
-						bgcolor1: this.bgcolor1,
-						bgcolor2: this.bgcolor2,
-						bgcolor3: this.bgcolor3,
-						bgcolor4: this.bgcolor4,
-						bgcolor5: this.bgcolor5,
-						bgcolor6: this.bgcolor6
                     }
                 }
             }));
@@ -97,59 +57,15 @@
 		get color() {
 			return this._shadowRoot.getElementById("ap_textcolor").value;
 		}
-		get value() {
-            return this._shadowRoot.getElementById("ap_selectedTop").value;
-        }
 		get fontsize() {
             return this._shadowRoot.getElementById("ap_fontsize").value;
         }
-		get bgcolor1() {
-			return this._shadowRoot.getElementById("ap_bgcolor1").value;
-		}
-		get bgcolor2() {
-			return this._shadowRoot.getElementById("ap_bgcolor2").value;
-		}
-		get bgcolor3() {
-			return this._shadowRoot.getElementById("ap_bgcolor3").value;
-		}
-		get bgcolor4() {
-			return this._shadowRoot.getElementById("ap_bgcolor4").value;
-		}
-		get bgcolor5() {
-			return this._shadowRoot.getElementById("ap_bgcolor5").value;
-		}
-		get bgcolor6() {
-			return this._shadowRoot.getElementById("ap_bgcolor6").value;
-		}
-        set color(v) {
+	    set color(v) {
             this._shadowRoot.getElementById("ap_textcolor").value = v;
-        }
-        set value(v) {
-            this._shadowRoot.getElementById("ap_selectedTop").value = v;
         }
 		set fontsize(v) {
             this._shadowRoot.getElementById("ap_fontsize").value = v;
         }
-		set bgcolor1(v) {
-            this._shadowRoot.getElementById("ap_bgcolor1").value = v;
-        }
-		set bgcolor2(v) {
-            this._shadowRoot.getElementById("ap_bgcolor2").value = v;
-        }
-		set bgcolor3(v) {
-            this._shadowRoot.getElementById("ap_bgcolor3").value = v;
-        }
-		set bgcolor4(v) {
-            this._shadowRoot.getElementById("ap_bgcolor4").value = v;
-        }
-		set bgcolor5(v) {
-            this._shadowRoot.getElementById("ap_bgcolor5").value = v;
-        }
-		set bgcolor6(v) {
-            this._shadowRoot.getElementById("ap_bgcolor6").value = v;
-        }
-
     }
-
-    customElements.define('chart-style', PyramidChartProperties);
+    customElements.define('chart-style', TachometerProperties);
 })();
